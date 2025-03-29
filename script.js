3485958f412ea07509652dad7216e93d3fb43c89
ItOnltz <hohohubbro@gmail.com>
2025-03-29 23:26:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -76,16 +76,21 @@ async function loading() {
         ". .",
         ". . ."
     ]
-    var i = 0
-    setInterval(function() {
-        if (text) {
-            if (i > 2) {
-                i = 0
+    if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        alert("Bro! This website only work for Pc");
+        window.location.href = "https://google.com";
+    } else {
+        var i = 0
+        setInterval(function() {
+            if (text) {
+                if (i > 2) {
+                    i = 0
+                }
+                text.innerHTML = `<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-aperture-icon lucide-aperture"><circle cx="12" cy="12" r="10"/><path d="m14.31 8 5.74 9.94"/><path d="M9.69 8h11.48"/><path d="m7.38 12 5.74-9.94"/><path d="M9.69 16 3.95 6.06"/><path d="M14.31 16H2.83"/><path d="m16.62 12-5.74 9.94"/></svg> </span> Loading ${ok[i]}`
+                i = i + 1
             }
-            text.innerHTML = `<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-aperture-icon lucide-aperture"><circle cx="12" cy="12" r="10"/><path d="m14.31 8 5.74 9.94"/><path d="M9.69 8h11.48"/><path d="m7.38 12 5.74-9.94"/><path d="M9.69 16 3.95 6.06"/><path d="M14.31 16H2.83"/><path d="m16.62 12-5.74 9.94"/></svg> </span> Loading ${ok[i]}`
-            i = i + 1
-        }
-    }, 500)
+        }, 500)
+    }
 }
 async function get() {
     try {
